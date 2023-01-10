6cfaf7b40814d5eef882a07c7ed9abebb5762822
Cadis Adrian <adicadis.ie@gmail.com>
2023-01-10 04:39:33 +0200

    Lab4

---
+++ Proiect_Medii_2/Models/VizualizareModele/ReprezentataDateIndex.cs
@@ -0,0 +1,11 @@
+﻿using Proiect_Medii_2.Models;
+using System.Security.Policy;
+
+namespace Proiect_Medii_2.Models.VizualizareModele
+{
+    public class ReprezentataDateIndex
+    {
+        public IEnumerable<Reprezentanta> Reprezentante { get; set; }
+        public IEnumerable<Masina> Masini { get; set; }
+    }
+}
